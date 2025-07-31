58aec499c0c29899291097fc3930bcf2471207f5
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-31 22:15:57 +0200

    Commited

--- app/src/main/java/com/example/loginapp2_signup_retrofit/LogInActivity.java
+++ app/src/main/java/com/example/loginapp2_signup_retrofit/LogInActivity.java
@@ -1,12 +1,35 @@
 package com.example.loginapp2_signup_retrofit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInActivity extends AppCompatActivity {
+    private Button b_in_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main); }
+        setContentView(R.layout.log_in_main);
+
+        b_in_back = findViewById(R.id.b_in_back);
+        setUpBackButton();
+    }
+
+    private void setUpBackButton() {
+        b_in_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        LogInActivity.this, MainActivity.class
+                );
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/loginapp2_signup_retrofit/SingUpActivity.java
+++ app/src/main/java/com/example/loginapp2_signup_retrofit/SingUpActivity.java
@@ -1,13 +1,32 @@
 package com.example.loginapp2_signup_retrofit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SingUpActivity extends AppCompatActivity {
+    private Button b_up_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sing_up_main);
+
+        b_up_back = findViewById(R.id.b_up_back);
+        setUpBackButton();
+    }
+
+    private void setUpBackButton() {
+        b_up_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SingUpActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
