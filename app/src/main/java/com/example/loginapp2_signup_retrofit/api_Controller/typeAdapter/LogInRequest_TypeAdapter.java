2dae4e2c618c786ac18fb848709f7b10189e930f
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-08-03 20:44:17 +0200

    Commited

--- app/src/main/java/com/example/loginapp2_signup_retrofit/api_Controller/typeAdapter/LogInRequest_TypeAdapter.java
+++ app/src/main/java/com/example/loginapp2_signup_retrofit/api_Controller/typeAdapter/LogInRequest_TypeAdapter.java
@@ -1,4 +1,38 @@
 package com.example.loginapp2_signup_retrofit.api_Controller.typeAdapter;
 
-public class LogInRequest_TypeAdapter {
+import com.example.loginapp2_signup_retrofit.api_Controller.LogInRequestController;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/*
+* convert to Json
+* */
+public class LogInRequest_TypeAdapter extends TypeAdapter<LogInRequestController> {
+
+    public LogInRequest_TypeAdapter() {
+
+    }
+
+    @Override
+    public void write(JsonWriter out, LogInRequestController value) throws IOException {
+        if (value == null) {
+            out.nullValue();
+            return;
+        }
+        out.beginObject();
+        out.name("email").value(value.getEmail());
+        out.name("password").value(value.getPassword());
+        out.endObject();
+    }
+
+    @Override
+    public LogInRequestController read(JsonReader in) throws IOException {
+         throw new UnsupportedEncodingException("Deserialization of LoginRequest is not supported.");
+    }
+
+
 }
--- app/src/main/java/com/example/loginapp2_signup_retrofit/api_Controller/typeAdapter/LogInResponse_TypeAdapter.java
+++ app/src/main/java/com/example/loginapp2_signup_retrofit/api_Controller/typeAdapter/LogInResponse_TypeAdapter.java
@@ -1,4 +1,55 @@
 package com.example.loginapp2_signup_retrofit.api_Controller.typeAdapter;
 
-public class LogInResponse_TypeAdapter {
+import com.example.loginapp2_signup_retrofit.api_Controller.LogInRequestController;
+import com.example.loginapp2_signup_retrofit.api_Controller.LogInResponseController;
+import com.example.loginapp2_signup_retrofit.api_Controller.impl.LogInResponseController_impl;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+/*
+ * convert to Json
+ * */
+public class LogInResponse_TypeAdapter extends TypeAdapter<LogInResponseController> {
+
+    public LogInResponse_TypeAdapter() {
+
+    }
+
+    @Override
+    public void write(JsonWriter out, LogInResponseController value) throws IOException {
+        if (value == null) {
+            out.nullValue();
+            return;
+        }
+        out.beginObject();
+        out.name("token").value(value.getToken());
+        out.name("message").value(value.getMessage());
+        out.name("isSuccess").value(value.getIsSuccess());
+        out.endObject();
+    }
+
+    @Override
+    public LogInResponseController read(JsonReader in) throws IOException {
+        LogInResponseController responseController = new LogInResponseController_impl();
+
+        in.beginObject();
+        while (in.hasNext()) {
+            String name = in.nextName();
+            if (name.equals("token")) {
+                responseController.setToken(in.nextString());
+            } else if (name.equals("message")) {
+                responseController.setMessage(in.nextString());
+            } else if (name.equals("isSuccess")) {
+                responseController.setIsSuccess(in.nextBoolean());
+            } else {
+                in.skipValue();
+            }
+        }
+        in.endObject();
+        return responseController;
+    }
+
 }
