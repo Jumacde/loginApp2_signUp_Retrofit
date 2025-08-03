9ed796b7219be48e6263e82c2b7acd1161438e10
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-08-03 20:55:51 +0200

    Commited

--- app/src/main/java/com/example/loginapp2_signup_retrofit/api_Controller/RetrofitClient.java
+++ app/src/main/java/com/example/loginapp2_signup_retrofit/api_Controller/RetrofitClient.java
@@ -1,16 +1,36 @@
 package com.example.loginapp2_signup_retrofit.api_Controller;
 
+import com.example.loginapp2_signup_retrofit.api_Controller.typeAdapter.LogInRequest_TypeAdapter;
+import com.example.loginapp2_signup_retrofit.api_Controller.typeAdapter.LogInResponse_TypeAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
     // input "ipconfig" on cmd and input here IPv4 adresse.
     // "http://IPv4-Adresse:8080/ "
-    private String BASE_URL = "http://172.22.243.244:8080/";
+    private static String BASE_URL = "http://172.22.243.244:8080/";
 
 
     public RetrofitClient() {
 
+    }
+
+    public static AuthService getAuthService() {
+        if (retrofit == null) {
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(LogInRequestController.class, new LogInRequest_TypeAdapter());
+            gsonBuilder.registerTypeAdapter(LogInResponseController.class, new LogInResponse_TypeAdapter());
+            Gson gson =  gsonBuilder.create();
+
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+                    GsonConverterFactory.create(gson)
+            ).build();
+        }
+        return retrofit.create(AuthService.class);
     }
 
 }
