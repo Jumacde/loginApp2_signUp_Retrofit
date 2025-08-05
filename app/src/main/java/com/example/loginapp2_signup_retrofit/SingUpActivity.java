29aed314393bcf27596536f68ff55dfe7eb286e6
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-08-05 17:16:09 +0200

    Commited

--- app/src/main/java/com/example/loginapp2_signup_retrofit/SingUpActivity.java
+++ app/src/main/java/com/example/loginapp2_signup_retrofit/SingUpActivity.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SingUpActivity extends AppCompatActivity {
-    private Button b_up_back;
+    private Button b_up_back, b_up_submit;
+    private EditText eT_Up_name, et_Up_email, et_Up_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sing_up_main);
+
+        eT_Up_name = findViewById(R.id.et_Up_name);
+        et_Up_email = findViewById(R.id.et_Up_email);
+        et_Up_password = findViewById(R.id.et_Up_password);
+
+        b_up_submit = findViewById(R.id.b_up_SingUp);
 
         b_up_back = findViewById(R.id.b_up_back);
         setUpBackButton();
